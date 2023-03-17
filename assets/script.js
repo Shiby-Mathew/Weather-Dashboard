b534aebc8db5e7139ecbebc29654c1a69067d330
Shiby-Mathew <shiby.ajay@gmail.com>
2023-03-17 15:04:11 +1100

    created html,css,.js file

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var searchEl = document.getElementById("search");
+
+
+function searchByCity(event) {
+  event.preventDefault();
+  var cityName = document.querySelector("#city").value;
+ 
+}
+
+
+
+
+searchEl.addEventListener("click", searchByCity);
+
