2813a8d3611a9842f8562fc360eed4f57df0cace
Shiby-Mathew <shiby.ajay@gmail.com>
2023-03-18 19:02:31 +1100

    added fetch functionality

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,8 @@ var searchEl = document.getElementById("search");
 
 // var requestUrl =
 //   "http://api.openweathermap.org/geo/1.0/direct?q=Sydney&limit=5&appid=cb4e1eb2ddfd04be05240608cc0d201b";
-//"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=cb4e1eb2ddfd04be05240608cc0d201b";
-//cb4e1eb2ddfd04be05240608cc0d201b
-//cb4e1eb2ddfd04be05240608cc0d201b
-//cb4e1eb2ddfd04be05240608cc0d201b0
+//"http://api.openweathermap.org/geo/1.0/reverse?lat=51.5098&lon=-0.1180&limit=5&appid=cb4e1eb2ddfd04be05240608cc0d201b";
+//9cf34e2a28704208b105434feb2d65c2
 
 function searchByCity(event) {
   event.preventDefault();
@@ -18,15 +16,50 @@ function searchByCity(event) {
 
 var getFeaturedRepos = function (cityName) {
   var requestUrl =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
-    " &limit=5&appid=cb4e1eb2ddfd04be05240608cc0d201b";
+    " &units=metric&limit=5&appid=cb4e1eb2ddfd04be05240608cc0d201b";
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+      var city = data.name;
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      //build current weather
+      var cityNameEl = document.createElement("h2");
+      var dateEl = document.createElement("p");
+      var weatherIconEl = document.createElement("img");
+      var tempEl = document.createElement("p");
+      var windEl = document.createElement("p");
+      var humdEl = document.createElement("p");
+      cityNameEl.textContent = city;
+      dateEl.textContent = dayjs().format("MM/DD/YYYY");
+      //dateEl.textContent = "Today's Date:";
+      //console.log(dateEl.textContent);
+      weatherIconEl.setAttribute(
+        "src",
+        `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+      );
+      tempEl.textContent = `Temp: ${data.main.temp}`;
+      windEl.textContent = `Wind: ${data.wind.speed}`;
+      humdEl.textContent = `Humidity: ${data.main.humidity}`;
+      document
+        .querySelector("#today")
+        .append(cityNameEl, dateEl, weatherIconEl, tempEl, windEl, humdEl);
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=cb4e1eb2ddfd04be05240608cc0d201b`
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          // Build forecast weather
+        });
     });
 };
 
