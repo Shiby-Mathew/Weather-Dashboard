6f1d2b63330b965816c8a9d0c61e8dad1a883741
Shiby-Mathew <shiby.ajay@gmail.com>
2023-03-21 09:44:32 +1100

    deleted unwanted items and added proper functions

--- assets/script.js
+++ assets/script.js
@@ -7,20 +7,18 @@ var dateEl = document.getElementById("date");
 var currentHeading = document.getElementById("current-heading");
 var currentWeather = document.getElementById("current-weather");
 
-// var requestUrl =
-//   "http://api.openweathermap.org/geo/1.0/direct?q=Sydney&limit=5&appid=cb4e1eb2ddfd04be05240608cc0d201b";
-//"http://api.openweathermap.org/geo/1.0/reverse?lat=51.5098&lon=-0.1180&limit=5&appid=cb4e1eb2ddfd04be05240608cc0d201b";
-//9cf34e2a28704208b105434feb2d65c2
-
+//Click event calling function
 function searchByCity(event) {
+  //check here
   event.preventDefault();
-  var cityName = document.querySelector("#city-search").value;
+  var city = document.querySelector("#city-search").value;
 
-  if (cityName) {
-    getFeaturedRepos(cityName);
+  if (city) {
+    getFeaturedRepos(city);
   }
 }
 
+//Get Current weather input from user
 var getFeaturedRepos = function (cityName) {
   var requestUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -33,100 +31,97 @@ var getFeaturedRepos = function (cityName) {
     .then(function (data) {
       console.log(data);
       var city = data.name;
-      var lat = data.coord.lat;
-      var lon = data.coord.lon;
-
+      var latitude = data.coord.lat;
+      var longitude = data.coord.lon;
       var todaysDate = dayjs().format("DD/MM/YYYY");
 
-      //display hidden block currentweather and forecast
+      //Display hidden block currentweather and forecast
       currentWeather.classList.remove("d-none");
       var forecast = document.getElementById("forecast");
       forecast.classList.remove("d-none");
 
+      //Display current weather
       currentHeading.textContent = "Current Weather in " + city;
       dateEl.textContent = `Today is :  ${todaysDate}`;
       var weatherIconEl = document.createElement("img");
       var iconLink = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
       weatherIconEl.setAttribute("src", iconLink);
-
-      // weatherIconEl.setAttribute(
-      //   "src",
-      //   `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
-      // );
-
       dateEl.append(weatherIconEl);
       tempEl.textContent = `Temp is:  ${data.main.temp} °C`;
       windEl.textContent = `Wind is:  ${data.wind.speed} km/h`;
-      humdEl.textContent = `Humidity is:  ${data.main.humidity} %`;
+      humdEl.textContent = `Humidity is:  ${data.main.humidity}%`;
 
-      fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=cb4e1eb2ddfd04be05240608cc0d201b`
-      )
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          console.log(data);
-
-          //forecast weather --First Day
-
-          var temp1 = document.getElementById("card1-temp");
-          var wind1 = document.getElementById("card1-wind");
-          var humid1 = document.getElementById("card1-humid");
-          //  var date1 = document.getElementById("card1-date");
-          var iconLink = `https://api.openweathermap.org/img/w/${data.list[1].weather[0].icon}.png`;
-          document.getElementById("card1-icon").src = iconLink;
-          //
-          // date1.textContent = `Date is:  ${data.list[1].dt_txt}`;
-          temp1.textContent = `Temp is:  ${data.list[1].main.temp} °C`;
-          wind1.textContent = `Wind is:  ${data.list[1].wind.speed} km/h`;
-          humid1.textContent = `Humidity is:  ${data.list[1].main.humidity} %`;
-
-          //forecast weather --second Day
-
-          var temp2 = document.getElementById("card2-temp");
-          var wind2 = document.getElementById("card2-wind");
-          var humid2 = document.getElementById("card2-humid");
-          var iconLink = `https://api.openweathermap.org/img/w/${data.list[9].weather[0].icon}.png`;
-          document.getElementById("card2-icon").src = iconLink;
-          temp2.textContent = `Temp is:  ${data.list[9].main.temp} °C`;
-          wind2.textContent = `Wind is:  ${data.list[9].wind.speed} km/h`;
-          humid2.textContent = `Humidity is:  ${data.list[9].main.humidity} %`;
-
-          //forecast weather --third Day
-
-          var temp3 = document.getElementById("card3-temp");
-          var wind3 = document.getElementById("card3-wind");
-          var humid3 = document.getElementById("card3-humid");
-          var iconLink = `https://api.openweathermap.org/img/w/${data.list[17].weather[0].icon}.png`;
-          document.getElementById("card3-icon").src = iconLink;
-          temp3.textContent = `Temp is:  ${data.list[17].main.temp} °C`;
-          wind3.textContent = `Wind is:  ${data.list[17].wind.speed} km/h`;
-          humid3.textContent = `Humidity is:  ${data.list[17].main.humidity} %`;
-
-          //forecast weather --fourth Day
-
-          var temp4 = document.getElementById("card4-temp");
-          var wind4 = document.getElementById("card4-wind");
-          var humid4 = document.getElementById("card4-humid");
-          var iconLink = `https://api.openweathermap.org/img/w/${data.list[25].weather[0].icon}.png`;
-          document.getElementById("card4-icon").src = iconLink;
-          temp4.textContent = `Temp is:  ${data.list[25].main.temp} °C`;
-          wind4.textContent = `Wind is:  ${data.list[25].wind.speed} km/h`;
-          humid4.textContent = `Humidity is:  ${data.list[25].main.humidity} %`;
-
-          //forecast weather --fifth Day
-
-          var temp5 = document.getElementById("card5-temp");
-          var wind5 = document.getElementById("card5-wind");
-          var humid5 = document.getElementById("card5-humid");
-          var iconLink = `https://api.openweathermap.org/img/w/${data.list[33].weather[0].icon}.png`;
-          document.getElementById("card5-icon").src = iconLink;
-          temp5.textContent = `Temp is:  ${data.list[33].main.temp} °C`;
-          wind5.textContent = `Wind is:  ${data.list[33].wind.speed} km/h`;
-          humid5.textContent = `Humidity is:  ${data.list[33].main.humidity} %`;
-        });
+      forecastWeather(latitude, longitude);
     });
 };
+
+//Fetch data for Forecast weather
+function forecastWeather(lat, lon) {
+  var forecastAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=cb4e1eb2ddfd04be05240608cc0d201b`;
+  fetch(forecastAPI)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      displayForecast(data);
+    });
+}
+
+//Display forecast weather for 5 days
+function displayForecast(data) {
+  //forecast weather --First Day
+  var temp1 = document.getElementById("card1-temp");
+  var wind1 = document.getElementById("card1-wind");
+  var humid1 = document.getElementById("card1-humid");
+  //  var date1 = document.getElementById("card1-date");
+  var iconLink = `https://api.openweathermap.org/img/w/${data.list[1].weather[0].icon}.png`;
+  document.getElementById("card1-icon").src = iconLink;
+  //
+  // date1.textContent = `Date is:  ${data.list[1].dt_txt}`;
+  temp1.textContent = `Temp is:  ${data.list[1].main.temp} °C`;
+  wind1.textContent = `Wind is:  ${data.list[1].wind.speed} km/h`;
+  humid1.textContent = `Humidity is:  ${data.list[1].main.humidity}%`;
+
+  //forecast weather --second Day
+  var temp2 = document.getElementById("card2-temp");
+  var wind2 = document.getElementById("card2-wind");
+  var humid2 = document.getElementById("card2-humid");
+  var iconLink = `https://api.openweathermap.org/img/w/${data.list[9].weather[0].icon}.png`;
+  document.getElementById("card2-icon").src = iconLink;
+  temp2.textContent = `Temp is:  ${data.list[9].main.temp} °C`;
+  wind2.textContent = `Wind is:  ${data.list[9].wind.speed} km/h`;
+  humid2.textContent = `Humidity is:  ${data.list[9].main.humidity}%`;
+
+  //forecast weather --third Day
+  var temp3 = document.getElementById("card3-temp");
+  var wind3 = document.getElementById("card3-wind");
+  var humid3 = document.getElementById("card3-humid");
+  var iconLink = `https://api.openweathermap.org/img/w/${data.list[17].weather[0].icon}.png`;
+  document.getElementById("card3-icon").src = iconLink;
+  temp3.textContent = `Temp is:  ${data.list[17].main.temp} °C`;
+  wind3.textContent = `Wind is:  ${data.list[17].wind.speed} km/h`;
+  humid3.textContent = `Humidity is:  ${data.list[17].main.humidity}%`;
+
+  //forecast weather --fourth Day
+  var temp4 = document.getElementById("card4-temp");
+  var wind4 = document.getElementById("card4-wind");
+  var humid4 = document.getElementById("card4-humid");
+  var iconLink = `https://api.openweathermap.org/img/w/${data.list[25].weather[0].icon}.png`;
+  document.getElementById("card4-icon").src = iconLink;
+  temp4.textContent = `Temp is:  ${data.list[25].main.temp} °C`;
+  wind4.textContent = `Wind is:  ${data.list[25].wind.speed} km/h`;
+  humid4.textContent = `Humidity is:  ${data.list[25].main.humidity}%`;
+
+  //forecast weather --fifth Day
+  var temp5 = document.getElementById("card5-temp");
+  var wind5 = document.getElementById("card5-wind");
+  var humid5 = document.getElementById("card5-humid");
+  var iconLink = `https://api.openweathermap.org/img/w/${data.list[33].weather[0].icon}.png`;
+  document.getElementById("card5-icon").src = iconLink;
+  temp5.textContent = `Temp is:  ${data.list[33].main.temp} °C`;
+  wind5.textContent = `Wind is:  ${data.list[33].wind.speed} km/h`;
+  humid5.textContent = `Humidity is:  ${data.list[33].main.humidity}%`;
+}
 
 searchEl.addEventListener("click", searchByCity);
